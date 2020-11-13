f3c935c460eaf41e2d9eb235c56a7966da0da4c9
burdissomattia <m.burdisso.0861@vallauri.edu>
2020-11-13 11:02:45 +0100

    aggiunto scrittura base al commento

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("#btnP3").click(newPage)
+});
+
+function newPage(){
+    window.location = "pag3.html";//nomme a caso ri una risorsa, a node interessa solo che sia gertita
+}
